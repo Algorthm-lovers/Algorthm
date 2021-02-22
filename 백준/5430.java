091922a227f2048bd9f2196123cfd378e594d14b
Coldlight <46393079+luseiit@users.noreply.github.com>
2021-02-22 13:08:55 +0900

    Update 5430.java

--- 백준/5430.java
+++ 백준/5430.java
@@ -1,49 +1,83 @@
-import java.util.*;
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for (int tc = 1; tc <= T; tc++) {
-            String p = sc.next();
-            int n = sc.nextInt();
-
-            String arrStr = sc.next();
-            Deque<Integer> deque = new LinkedList<>();
-            for (String s : arrStr.substring(1, arrStr.length() - 1).split(","))
-                if (!s.equals(""))
-                    deque.add(Integer.valueOf(s));
-
-            System.out.println(ac(deque, p));
-        }
+    public static void main(String[] args) throws IOException {
+        new Main().run();
     }
+    private void run() throws IOException{
+        String input = "";
+        String output = "";
+        boolean isReverse = false; //true시에 순서가 바뀐것 false 시에 순서가 그대로인것
+        boolean isError = false;
+        int test_case = 0;
+        char[] function;
+        int array_length = 0;
 
-    static String ac(Deque<Integer> deque, String commands) {
-        boolean reverse = false;
+        Deque<String> deque = new LinkedList<String>(); // deque 선언. 배열 뒤집는 경우에는 뒤에서부터 뺀다
+                                                        // R이 수행된 이후에 첫번째 숫자 버리는 경우에는 맨 뒤의 문자 버린다
 
-        for (char command : commands.toCharArray()) {
-            if (command == 'R')
-                reverse = !reverse;
-            else {
-                if (deque.size() == 0)
-                    return "error";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-                if (reverse)
-                    deque.removeLast();
-                else
-                    deque.removeFirst();
+
+        test_case = Integer.parseInt(br.readLine()); // test case의 수 입력 loop에 사용됨.
+        if(test_case > 101){ // 테스트 케이스의 개수는 최대 100이다.
+            return;
+        }
+        for(int i = 0; i< test_case; i++){
+            function = br.readLine().toCharArray(); // 수행될 function 입력 <char 형 배열로>
+            array_length = Integer.parseInt(br.readLine()); // 배열에 들어있는 수의 갯수 n의 입력
+
+            input = br.readLine();
+            input = input.substring(1,input.length()-1);
+            StringTokenizer st = new StringTokenizer(input,",");
+
+            while(st.hasMoreTokens()){
+                deque.add(st.nextToken()); // deque에 값 삽입 완료
+            }
+
+            for(int j = 0; j < function.length; j++){ // function 수행
+
+                if(function[j] == 'R'){
+                    isReverse = !isReverse;
+                }
+                else{
+                    if(deque.isEmpty()){
+                        System.out.println("error");
+                        isError = true;
+                        break;
+                    }
+                }
+                if(isReverse){
+                    deque.pollLast();
+                }
+                else{
+                    deque.pollFirst();
+                }
+            }
+            if(!isError) {
+                System.out.print("[");
+                if (isReverse) {
+                    while (deque.size() > 1) {
+                        System.out.print(deque.pollLast() + ",");
+                    }
+                } else {
+                    while (deque.size() > 1) {
+                        System.out.print(deque.pollFirst());
+                    }
+                }
+                if (deque.size() != 0) {
+                    System.out.print(deque.getFirst());
+                }
+                System.out.println("]");
             }
         }
 
-        StringBuilder sb = new StringBuilder("[");
-        while (!deque.isEmpty()) {
-            sb.append(reverse ? deque.removeLast() : deque.removeFirst());
-            if (deque.size() != 0)
-                sb.append(',');
-        }
-        sb.append(']');
-
-        return sb.toString();
     }
 }
