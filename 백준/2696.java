5fa9c3a81f2885092d22adbe644d4e211375b132
Coldlight <46393079+luseiit@users.noreply.github.com>
2021-02-25 17:01:08 +0900

    2021-02-25 may be final

--- 백준/2696.java
+++ 백준/2696.java
@@ -10,10 +10,12 @@ public class Main
 		
 		int test_case = 0; // test_case1
 		int test_case2 = 0;
+
 		test_case = Integer.parseInt(bf.readline()); // input test_case's count
 		
 		for(int i = 0; i< test_case; i++){
 		    test_case2 = Integer.parseInt(bf.readline()); // input test_case2(heap's size)
+		    int[] middle = new int[test_case2/2+1]; // middle 값을 저장할 int array
 		    PriorityQueue<Integer> heap = new PriorityQueue<Integer>(); // 최소 heap 선언
 		    PriorityQueue<Integer> heap2 = new PriorityQueue<Integer>(Collections.reverseOrder());
 		                                                                // 최대 heap 선언 
@@ -25,12 +27,18 @@ public class Main
 		    for(int i = 0; i < test_case2/2 ; i+=2){ // 홀수인 index마다 출력 시키기로한다.
 		        System.out.println(test_case2/2 + 1); // test_case의 중간값이 몇 개 인지 출력한다.
 		                                               // ex) text_case2 = 9 -> 5
-		        int middle = heap.peek() + heap2.peek()
+		        middle[i] = (heap.peek() + heap2.peek()) / 2;
 		    }
+		    for(int i = 0 ; i < middle.length; i++){
+		        System.out.print(middle[i]); // middle 값만 출력하고 
+		    }
+		    System.out.println(""); // 한줄 띄운다.
 		    
-		    heap.clear(); // 새로운 값을 입력받기 위해서 heap을 정리한다.
+		    heap.clear(); // 새로운 값을 입력받기 위해서 heap을 정리한다. ( 최소 Heap ) 
+		    heap2.clear(); // 새로운 값을 입력받기 위해서 heap을 정리한다. ( 최대 Heap)
 		}
-
+		
+		return;
 		
 	}
 }
