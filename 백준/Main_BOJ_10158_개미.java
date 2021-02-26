496dcc9664ab1b32723b70412b39dea70d826bd2
SeYeonCho <46417206+SeYeonCho@users.noreply.github.com>
2021-02-26 11:40:59 +0900

    BOJ_10158_개미_v3_solved
    BOJ_10158_개미_SILVER_5
    11740KB / 88ms
    
    // x와 y좌표를 따로 생각 --> 비슷한 문제들에서 적용 가능한 중요 개념
    // x 방향 바꾸는 조건 오타 수정
    // 시작지점이 x = 0 또는 y = 0인경우 오류 발생할 수 있지만 해당 문제의 조건은 0 < p < w, 0 < q < h 이므로 고려하지 않아도 됨

--- 백준/Main_BOJ_10158_개미.java
+++ 백준/Main_BOJ_10158_개미.java
@@ -1,11 +1,14 @@
 package BOJ_10158_개미;
-// 사이클을 구해 나눠주는 방식을 써보았지만 여전히 시간초과
+
+// x와 y좌표를 따로 생각
+// x 방향 바꾸는 조건 오타 수정
+// 시작지점이 x = 0 또는 y = 0인경우 오류 발생할 수 있지만 해당 문제의 조건은 0 < p < w, 0 < q < h 이므로 고려하지 않아도 됨
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_BOJ_10158_개미 {
-	static int w, h, p, q, t, loop;
+public class Main_BOJ_10158_개미3 {
+	static int w, h, t;
 	static int x, y, dx = 1, dy = 1;
 
 	public static void main(String[] args) throws Exception {
@@ -14,23 +17,22 @@ public class Main_BOJ_10158_개미 {
 		w = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		p = Integer.parseInt(st.nextToken());
-		q = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		t = Integer.parseInt(st.nextToken());
-		
-		x = p; y = q;
-		for(int i = 0; i < t; i++) {
-			if(x==w || w==0) dx *= -1;
-			if(y==h || y==0) dy *= -1;
-			
+
+		for (int i = 0; i < t % (2 * w); i++) {
+			if (x == w || x == 0) dx *= -1;
+
 			x += dx;
+
+		}
+		for (int i = 0; i < t % (2 * h); i++) {
+			if (y == h || y == 0) dy *= -1;
+
 			y += dy;
-			
-			if(i!=0 && p == x && q ==y) {
-				t %= i;
-				break;
-			}
+
 		}
 
 		StringBuilder sb = new StringBuilder();
