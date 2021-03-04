c3681087a6b218254482e3aaac242f96f21f4265
SeYeonCho <46417206+SeYeonCho@users.noreply.github.com>
2021-03-05 02:03:09 +0900

    Update Main_BOJ_10819_차이를최대로.java
    BOJ_10819_차이를최대로_SILVER_2
    12984KB / 72ms
    // 완탐으로 풀 수 있는 문제이지만 수학적으로 문제의 해답을 찾아서 알고리즘에 적용
    // 허점이 발견되어서 수정 (반례 찾는 것이 힘들었음)
    // 디버깅 중 쓰지 않는 0번째 index가 정렬 과정에서 뒤섞이게 되는 것을 발견하여 수정

--- 백준/Main_BOJ_10819_차이를최대로.java
+++ 백준/Main_BOJ_10819_차이를최대로.java
@@ -1,31 +1,43 @@
 package BOJ_10819_차이를최대로;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_BOJ_10819_차이를최대로 {
+public class Main_BOJ_10819_차이를최대로2 {
 	static int N;
 	static int[] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1];	// 1번째 ~ N번 index까지 사용
+		arr = new int[N+1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr[0] = -100;	// 쓰지 않는 0번째 index에 최소값부여 (input의 최소값이 -100이므로 -100으로 초기화해도 됨)
 		for(int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr);
-		int half = N/2 + N%2;	// 중앙부터 시작 (시작은 중앙부터 오른쪽 끝으로)
-		int current = half, start = 1, end = N, sum = 0;
-		while(true) {	// 양끝단의 점이 중앙으로 되돌아올때까지 루프
-			sum += Math.abs(arr[current] - arr[end]); // 현재위치에서 오른쪽 끝까지 거리를 더해줌
-			current = end--;	// 현재 위치를 오른쪽 끝으로 바꾸고, 다음 오른쪽 끝 index를 1 줄여준다
-			if(start == half) break;	// 왼쪽 끝 index가 처음 위치로 돌아왔으면 break;
-			sum += Math.abs(arr[current] - arr[start]);	// 현재위치에서 왼쪽 끝까지 거리를 더해줌
-			current = start++;	// 현재 위치를 왼쪽 끝으로 바꾸고, 다음 왼쪽 끝 index를 1 늘려준다
-			if(end == half) break;	// 오른쪽 끝 index가 처음 위치로 되돌아왔으면 break;
+		int half = N/2 + N%2;
+		int cur = half, start = 1, end = N, sum = 0;
+		while(true) {
+			sum += Math.abs(arr[cur] - arr[end]); 
+			cur = end--;
+			if(start == half) break;
+			sum += Math.abs(arr[cur] - arr[start]);
+			cur = start++;
+			if(end == half) break;
+		}
+		if(N%2==1) {	// N이 홀수개일때는 중앙에서 왼쪽으로 시작하는 경우도 계산해서, 더 큰쪽을 반환하도록 해야함
+			cur = half; start = 1; end = N; int sum2 = 0;
+			while(true) {
+				sum2 += Math.abs(arr[cur] - arr[start]);
+				cur = start++;
+				if(end == half) break;
+				sum2 += Math.abs(arr[cur] - arr[end]); 
+				cur = end--;
+				if(start == half) break;
+			}
+			sum = Math.max(sum, sum2);
 		}
 		System.out.println(sum);
 	}
