d5f36796837dd4cbb8846b336dbf5fb785101fe5
SeYeonCho <46417206+SeYeonCho@users.noreply.github.com>
2021-03-06 17:45:55 +0900

    BOJ_1946_신입사원; ver1.3
    // ver1.2에서는 이중for문에서의 중첩을 없애어 O(n)의 시간복잡도 안에서 계산하도록 만들었다 (시간초과)
    // ver1.3에서는 리스트에서의 삭제연산이 시간복잡도를 크게 한다고 판단하여 삭제연산 대신 count 적용했지만 시간초과

--- 백준/Main_BOJ_1946_신입사원.java
+++ 백준/Main_BOJ_1946_신입사원.java
@@ -1,12 +1,12 @@
 package BOJ_1946_신입사원;
-// 2중 for문에서 for문 중첩을 한번만 돌도록 바꾸어 주었지만 여전히 시간초과
+// 리스트에서의 삭제연산이 시간복잡도를 크게 한다고 판단하여 삭제연산 대신 count 적용했지만 시간초과
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-public class Main_BOJ_1946_신입사원2 {
+public class Main_BOJ_1946_신입사원3 {
 	static class order implements Comparable<order>{
 		int order1, order2;
 		
@@ -41,12 +41,15 @@ public class Main_BOJ_1946_신입사원2 {
 			Collections.sort(list); // 1차 서류심사(석차1)에 대한 내림차순으로 정렬
 			
 			int min = list.get(list.size()-1).order2;
+			int cnt = 1;	// 최초 1명은 비교없이 시작(석차1에대해 1등이기때문)
 			for(int i = list.size()-1; i >= 0; i--) {
-				if(min < list.get(i).order2) list.remove(i);
-				else min = list.get(i).order2;
+				if(min > list.get(i).order2) {
+					cnt++;
+					min = list.get(i).order2;
+				}
 			}
 			
-			System.out.println(list.size());
+			System.out.println(cnt);
 			
 		}
 	}
